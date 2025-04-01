1a12c9bedf1bf33e45b786d713014c597732b7af
ayush gupta <aerospace141@gmail.com>
2025-04-02 00:45:53 +0530

    Update signup.js

--- src/components/user_auth/signup.js
+++ src/components/user_auth/signup.js
@@ -197,7 +197,7 @@ return (
             />
           </div>
         ))}
-        <ReCAPTCHA ref={recaptchaRef} sitekey="6Lc4pAYrAAAAACOdhs19wnUNpe8MQD_2uzZMcHQY" executeAsync={setRecaptchaValue}             size="invisible"
+        <ReCAPTCHA ref={recaptchaRef} sitekey="6LdRtgYrAAAAACt-P9haO2AiBXexb1lie07wK3Hg" executeAsync={setRecaptchaValue}             
  />
         <button type="submit" className="auth-button" disabled={!passwordMatch || loading}>
           {loading ? "Processing..." : "Sign Up"}
